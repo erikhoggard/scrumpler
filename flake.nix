0cf5145fe95692c7058989bf798a4ed30e330d64
Erik Hoggard <admin@erikhoggard.com>
2026-02-07 14:39:28 -0500

    Use nixos-unstable for consistency

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Scrumpler - Audio sample processor for experimental music production";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
